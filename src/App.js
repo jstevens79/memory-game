cda5235eefe22b8bb9d7dda79175b2f06c6c7e02
jstevens79 <jstevens79@users.noreply.github.com>
2019-06-03 18:22:36 -0500

    adding in randomization at start and on click

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Card from './components/Card'
+import styles from './styles/index.module.scss'
+
+const CardsArray = [1,2,3,4,5,6,7,8,9]
 
 function App() {
-  const [cards, setCards] = useState([1,2,3,4,5])
+  const [cards, setCards] = useState([])
   const [score, setScore] = useState(0)
   const [clicked, setClicked] = useState([])
+
+  const shuffle = arr => {
+    const Shuffled = arr.map(a => [Math.random(), a])
+      .sort((a, b) => a[0] - b[0])
+      .map(a => a[1])
+    return Shuffled
+    
+  }
 
   const clickHandler = num => {
     if (!clicked.includes(num)) {
       setScore(score + 1)
       const newClicked = [...clicked, num]
       setClicked(newClicked)
+      setCards(shuffle(CardsArray))
     } else {
       setScore(0)
       setClicked([])
+      setCards(shuffle(CardsArray))
     }
   }
-  console.log(clicked)
+
+  useEffect(() => {
+    setCards(shuffle(CardsArray))
+  }, [])
+
+
   return (
     <div className="App">
       <h1>Score: {score}</h1>
-      {cards.map(card => {
-        return <Card key={card} id={card} clickHandler={clickHandler} />
-      })}
+      <div className={styles.cardContainer}>
+        {cards.map(card => {
+          return <Card key={card} id={card} clickHandler={clickHandler} />
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import style from './_card.module.scss'
 
 function Card({id, clickHandler}) {
-  return <div className="card" onClick={() => clickHandler(id)}>Card {id}</div>
+  return <div className={style.card} onClick={() => clickHandler(id)}>Card {id}</div>
 }
 
 export default Card
