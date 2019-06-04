9b3faa700706f319cf045e2e9ec7e00104b1dd4b
jstevens79 <jstevens79@users.noreply.github.com>
2019-06-04 16:46:19 -0500

    Reworking layout a bit with CSS grid

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ function Card({data, clickHandler, displayed}) {
       <div
         className={`${style.cardContent} ${(displayed) ? style.shown : ''}`}
         onClick={() => clickHandler(data.id)}>
-        <img src={`img/${data.img}`} alt={'hey'}></img>
+        <img src={`img/${data.img}`} alt={data.alt}></img>
       </div>
     </div>
   )
